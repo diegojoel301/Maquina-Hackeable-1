e0f2b2a3e8ba393bc6801a660340af5816938578
Diego Joel Condori Quispe <93290842+diegojoel301@users.noreply.github.com>
2022-02-25 07:12:58 +0400

    Update admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -134,8 +134,8 @@
                   
  <?php 
     $servername = "127.0.0.1";
-    $username = "root";
-    $password = "";
+    $username = "admin";
+    $password = "afM0aty6AkBB";
     $dbname = "complexdb";
 
 
