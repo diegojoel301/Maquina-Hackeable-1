625769f21c6fade135e6cd5e07d0a08d44379d1f
Diego Joel Condori Quispe <93290842+diegojoel301@users.noreply.github.com>
2022-02-26 19:14:08 +0400

    Update edit_info.php

--- php/edit_info.php
+++ php/edit_info.php
@@ -1,8 +1,8 @@
 <?php
     session_start();
     $servername = "localhost";
-    $username = "admin";
-    $passworddb = "afM0aty6AkBB";
+    $username = "root";
+    $passworddb = "";
     $dbname = "complexdb";
     $email = $_SESSION["email"];
     $user_id = $_SESSION["user_id"];
