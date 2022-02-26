39ca1b97ef1858e7e82ddb0f24ad44008d3eefce
Diego Joel Condori Quispe <93290842+diegojoel301@users.noreply.github.com>
2022-02-26 19:19:14 +0400

    Update seats_avail_logic.php

--- php/seats_avail_logic.php
+++ php/seats_avail_logic.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 $servername = "127.0.0.1";
-$username = "admin";
-$password = "afM0aty6AkBB";
+$username = "root";
+$password = "";
 $dbname = "complexdb";
 $_SESSION['num_tickets'] = $_POST['num_tickets'];
 // echo $_SESSION['num_tickets'];
