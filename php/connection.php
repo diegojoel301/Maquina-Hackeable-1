3ed789ac1549e09ba545b03a2e9610181d760876
Diego Joel Condori Quispe <93290842+diegojoel301@users.noreply.github.com>
2022-02-26 19:13:24 +0400

    Update connection.php

--- php/connection.php
+++ php/connection.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 $servername = "127.0.0.1";
-$username = "admin";
-$password = "afM0aty6AkBB";
+$username = "root";
+$password = "";
 $dbname = "complexdb";
 $today = strtotime("Yesterday"); //???? Why won't it work for today
 $recemail = $_POST["email"];                      
